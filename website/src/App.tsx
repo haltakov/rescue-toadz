23b21957f7884e1b13912a14fb13b7c654c7d5e7
Vladimir Haltakov <vladimir.haltakov@gmail.com>
2022-03-24 23:45:11 +0100

    Integrate Plausible

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,6 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import PageLayout from "./components/PageLayout";
 
+import Plausible from "plausible-tracker";
+
+const { enableAutoPageviews } = Plausible({
+    domain: "rescue-toadz.netlify.app",
+    apiHost: "/plausible",
+});
+
+enableAutoPageviews();
+
 function App() {
     return (
         <BrowserRouter>
